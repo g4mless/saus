418c6a38df17ba22dde617cd98f381b1a96e6ba0
g4mless <g4mless@proton.me>
2025-09-26 19:38:18 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const welcomeStrings = [
+  'Hello Hono!'
+]
+
+app.get('/', (c) => {
+  return c.text(welcomeStrings.join('\n\n'))
+})
+
+export default app
